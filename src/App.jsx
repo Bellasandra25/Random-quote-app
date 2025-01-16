b68e7065f0622047e4ddd49c92b86a0f67193aac
Sandra Odubela <odubelaoluwadamilola@gmail.com>
2025-01-16 10:57:32 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import { useState, useEffect } from 'react'
 const App = () => {
 const[quote , setQuote] = useState('')
 const [author, setAuthor] = useState('')  
-const [loading, isLoading] = useState(false)
+const [loading, setLoading] = useState(false)
 
 const fetchQuote = async () => {
-  isLoading(true);
+  setLoading(true);
   try{
     const fetch= await axios.get('https://api.quotable.io/random')
-   setQuote(fetch.data.content);
-   setAuthor(fetch.data.author);
+    setTimeout(() => {
+      setQuote(res.data.content);
+      setAuthor(res.data.author); 
+      setLoading(false); 
+    }, 1000);
+   console.log(" fetched the quote")
   }catch(error){
     console.log(error);
+    setQuote('An error occured');
   }
-  isLoading(false);
+  setLoading(false);
 };
 
 
@@ -26,10 +31,16 @@ useEffect(()=>{
   return (
     <section className='text-center p-4 bg-grey-500 flex justify-center items-center text-white '>
       <h1>Random Quote Generator </h1>
+      {loading ? (
+          <div
+            className="loader border-4 border-blue-500 border-t-transparent rounded-full w-12 h-12 animate-spin"
+          ></div>
+        ) : (
   <textarea name="randomquote"
-   className='w-1/2 h-32 p-2 text-black rounded-lg' 
-   value={`${quote}\n- ${author}`} readOnly>{loading ? 'loading...' : '{quote}\n- {author}'}
+   className='w-1/2 h-32 p-2 text-black rounded-lg border' 
+   value={loading ? 'loading...' :`${quote}\n- ${author}`} readOnly>
 </textarea>
+)}
 <button
 className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
 onClick={() => fetchQuote()}
@@ -42,7 +53,7 @@ onClick={() => {
   const tweetUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
   window.open(tweetUrl, '_blank');
 }
-}
+}disabled = {!quote|| !author}
 >
   tweet quote
 </button>
